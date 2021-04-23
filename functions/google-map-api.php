59ec46f84c095c2bf36b0b5d2f457ef0c35c1bae
Matt <matt@mspacecreative.com>
2021-04-23 13:38:36 -0300

    c

--- functions/google-map-api.php
+++ functions/google-map-api.php
@@ -1,7 +1,9 @@
 <?php
 function googleMapAPIScript() {
+	/*
 	wp_register_script('aa_js_googlemaps_script', 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAMjm_0er-ybuWcVoPXYLSr82mn6bvNqCY', array('jquery'), null);
 	wp_enqueue_script('aa_js_googlemaps_script');
+	*/
 	
 	wp_register_script('marker-cluster-script', 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js', array('jquery'), null, true);
 	wp_enqueue_script('marker-cluster-script');
