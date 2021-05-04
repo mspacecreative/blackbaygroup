0af02052a553f76f8e570034291d849b378770b0
Matt <matt@mspacecreative.com>
2021-05-04 16:09:42 -0300

    c

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -11,20 +11,14 @@ function dynamic_field_values ( $tag, $unused ) {
         'order' => 'ASC',
         'meta_query' => array(
         	array(
-        		'relation' => 'OR',
-        		array(
-			        'key' => 'exclude_from_list',
-			        'value' => true,
-		    	),
-		    	array (
-		    		'relation' => 'OR',
-			    	array(
-				        'key' => 'exclude_listing_from_drop_down',
-				        'value' => false,
-			    	)
-			    )
-        	)
-		)
+		        'key' => 'exclude_from_list',
+		        'value' => true,
+	    	),
+	    	array(
+		        'key' => 'exclude_listing_from_drop_down',
+		        'compare' => 'NOT EXISTS',
+	    	),
+		),
     );
 
     $custom_posts = get_posts($args);
