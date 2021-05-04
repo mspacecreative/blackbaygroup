2d866fef144548999ef119d9c09eefd83df8dbe2
Matt <matt@mspacecreative.com>
2021-05-04 14:36:03 -0300

    c

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -11,11 +11,14 @@ function dynamic_field_values ( $tag, $unused ) {
         'orderby'       => 'title',
         'order'         => 'ASC',
         'meta_query'=> array(
-        	'relation' => 'OR',
-			array(
+        	array(
                 'key' => 'exclude_listing_from_drop_down',
                 'compare' => 'NOT EXISTS'
             ),
+           	array(
+	            'key' => 'exclude_from_list',
+	            'compare' => 'EXISTS'
+	        ),
 		),
     );
 
