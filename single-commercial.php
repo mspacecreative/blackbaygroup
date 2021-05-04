83a0a38f1dae15028f957c394e1b53da2b6b6236
Matt <matt@mspacecreative.com>
2021-05-04 09:14:46 -0300

    Added map, removed landlord reference button

--- single-commercial.php
+++ single-commercial.php
@@ -87,14 +87,11 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 				<!-- /UNIT INFO -->
 				
 				<!-- CTA BUTTONS -->
-				<div class="button light cta-button">
-					<a href="<?php echo home_url('contact'); ?>/tenant-rental-application/">Landlord Reference</a>
-				</div>
+				<?php if( get_field('more_info_button') ): ?>
 				<div class="button light">
-					<?php if( get_field('more_info_button') ): ?>
 					<a href="<?php the_field('more_info_button'); ?>" target="_blank">More Information</a>
-					<?php endif; ?>
 				</div>
+				<?php endif; ?>
 				<!-- END CTA BUTTONS -->
 				
 				<!-- PAGINATION -->
@@ -137,6 +134,110 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 				<?php endif; ?>
 				
 				<!-- GOOGLE MAP -->
+				<style type="text/css">
+				.single .acf-map {
+				    width: 100%;
+				    height: 400px;
+				    border: #ccc solid 1px;
+				    margin: 20px 0;
+				}
+				
+				.acf-map img {
+				   max-width: inherit !important;
+				}
+				</style>
+				<script type="text/javascript">
+				(function( $ ) {
+				
+					function initMap( $el ) {
+					
+					    // Find marker elements within map.
+					    var $markers = $el.find('.marker');
+					
+					    // Create gerenic map.
+					    var mapArgs = {
+					        zoom        : $el.data('zoom') || 16,
+					        mapTypeId   : google.maps.MapTypeId.ROADMAP
+					    };
+					    var map = new google.maps.Map( $el[0], mapArgs );
+					
+					    // Add markers.
+					    map.markers = [];
+					    $markers.each(function(){
+					        initMarker( $(this), map );
+					    });
+					
+					    // Center map based on markers.
+					    centerMap( map );
+					
+					    // Return map instance.
+					    return map;
+					}
+					
+					function initMarker( $marker, map ) {
+					
+					    // Get position from marker.
+					    var lat = $marker.data('lat');
+					    var lng = $marker.data('lng');
+					    var latLng = {
+					        lat: parseFloat( lat ),
+					        lng: parseFloat( lng )
+					    };
+					
+					    // Create marker instance.
+					    var marker = new google.maps.Marker({
+					        position : latLng,
+					        map: map
+					    });
+					
+					    // Append to reference for later use.
+					    map.markers.push( marker );
+					
+					    // If marker contains HTML, add it to an infoWindow.
+					    if( $marker.html() ){
+					
+					        // Create info window.
+					        var infowindow = new google.maps.InfoWindow({
+					            content: $marker.html()
+					        });
+					
+					        // Show info window when marker is clicked.
+					        google.maps.event.addListener(marker, 'click', function() {
+					            infowindow.open( map, marker );
+					        });
+					    }
+					}
+					
+					function centerMap( map ) {
+					
+					    // Create map boundaries from all map markers.
+					    var bounds = new google.maps.LatLngBounds();
+					    map.markers.forEach(function( marker ){
+					        bounds.extend({
+					            lat: marker.position.lat(),
+					            lng: marker.position.lng()
+					        });
+					    });
+					
+					    // Case: Single marker.
+					    if( map.markers.length == 1 ){
+					        map.setCenter( bounds.getCenter() );
+					
+					    // Case: Multiple markers.
+					    } else{
+					        map.fitBounds( bounds );
+					    }
+					}
+					
+					// Render maps on page load.
+					$(document).ready(function(){
+					    $('.acf-map').each(function(){
+					        var map = initMap( $(this) );
+					    });
+					});
+				
+				})(jQuery);
+				</script>
 				<?php 
 				
 				$location = get_field('location_map');
@@ -144,7 +245,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 				if( !empty($location) ):
 				?>
 				<div class="acf-map">
-					<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+					<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
 				</div>
 				<?php endif; ?>
 				<!-- END GOOGLE MAP -->
