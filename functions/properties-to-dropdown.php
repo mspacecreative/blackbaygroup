7e7b247597815c4e83ff7f5ba6384b87ec5f459c
Matt <matt@mspacecreative.com>
2021-05-04 15:57:23 -0300

    c

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -11,12 +11,8 @@ function dynamic_field_values ( $tag, $unused ) {
         'order' => 'ASC',
         'meta_query' => array(
         	array(
-		        'key' => 'exclude_listing_from_drop_down',
-		        'compare' => 'NOT EXISTS'
-	    	),
-	    	array(
 		        'key' => 'exclude_from_list',
-		        'compare' => '!='
+		        'compare' => 'EXISTS'
 	    	),
 		),
     );
