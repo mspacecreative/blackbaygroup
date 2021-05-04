73a13b1e5c15b3c94dd887179328bc8995ac2bee
Matt <matt@mspacecreative.com>
2021-05-04 18:37:09 -0300

    C

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -14,6 +14,7 @@ function dynamic_field_values ( $tag, $unused ) {
         	array(
 			    'key' => 'exclude_listing_from_drop_down',
 			    'value' => false,
+			    'compare' => 'EXISTS'
 			)
 		)
     );
