dc9c0b578dbbb6350a23d7578a19ff0426952a21
Matt <matt@mspacecreative.com>
2021-05-04 14:55:49 -0300

    C

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -14,12 +14,8 @@ function dynamic_field_values ( $tag, $unused ) {
         	array(
         		'relation' => 'AND',
         		array(
-	        		'key' => 'exclude_listing_from_drop_down',
-	                'compare' => 'NOT EXISTS'
-        		),
-        		 array(
-                'key' => 'exclude_from_list',
-                'compare' => 'EXISTS'
+	                'key' => 'exclude_from_list',
+	                'compare' => 'EXISTS'
             	),
             ),
 		),
