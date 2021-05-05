47a666ffc3769cda5d2a3d5ddaf583c5f6870e0e
Matt <matt@mspacecreative.com>
2021-05-05 09:13:30 -0300

    c

--- functions/properties-to-dropdown.php
+++ functions/properties-to-dropdown.php
@@ -9,13 +9,12 @@ function dynamic_field_values ( $tag, $unused ) {
         'numberposts' => -1,
         'post_type' => 'residential',
         'order' => 'ASC',
-        //'meta_query' => array(
-        	//array(
-			  //  'key' => 'exclude_listing_from_drop_down',
-			    //'value' => '1',
-			   // 'compare' => '!='
-		//	)
-		//)
+        'meta_query' => array(
+        	array(
+				'key' => 'include_listing_in_drop_down',
+			   'value' => '1'
+			)
+		)
     );
 
     $custom_posts = get_posts($args);
