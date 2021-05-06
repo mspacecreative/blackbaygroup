cacee1cb5bc2c3c7e89d9eb8d022c3477d4bab05
Matt <matt@mspacecreative.com>
2021-05-06 11:32:30 -0300

    c

--- archive-residential.php
+++ archive-residential.php
@@ -14,7 +14,7 @@
 				'meta_query'=> array(
 					array(
                     	'key' => 'exclude_from_list',
-                    	'value' => '1';
+                    	'value' => '1',
                     	'compare' => '!='
                     ),
 				),
