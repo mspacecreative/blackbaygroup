86f406c3f699b99531ff78c96add445a507513d8
Matt <matt@mspacecreative.com>
2021-05-06 13:32:58 -0300

    c

--- includes/residential-listings-map.php
+++ includes/residential-listings-map.php
@@ -85,60 +85,58 @@
 
 	function initMap( $el ) {
 	
-	    // Find marker elements within map.
-	    var $markers = $el.find('.marker');
-		//var halifax = { lat: 43.6386507, lng: -65.7209915 };
+		var $markers = $el.find('.marker');
 	
-	    // Create gerenic map.
-	    var mapArgs = {
-	        zoom        : $el.data('zoom') || 10,
-	        mapTypeId   : google.maps.MapTypeId.ROADMAP
-	    };
-	    var map = new google.maps.Map( $el[0], mapArgs );
+		var args = {
+			zoom		: 16,
+			center		: new google.maps.LatLng(0, 0),
+			mapTypeId	: google.maps.MapTypeId.ROADMAP
+		};
 	
-	    // Add markers.
-	    map.markers = [];
-	    $markers.each(function(){
-	        initMarker( $(this), map );
-	    });
+		// create map
+		var map = new google.maps.Map( $el[0], args);
 	
-	    // Center map based on markers.
-	    centerMap( map );
+		// add a markers reference
+		map.markers = [];
+	
+		// add markers
+		$markers.each(function(){
+			add_marker( $(this), map );
+		});
+	
+		// center map
+		center_map( map );
 		
 		// add marker cluster
 		markerCluster( map.markers, map );
 	
-	    // Return map instance.
-	    return map;
+		// return
+		return map;
 	}
 	
 	var activeInfoWindow;
 	function initMarker( $marker, map ) {
 	
-	    // Get position from marker.
-	    var lat = $marker.data('lat');
-	    var lng = $marker.data('lng');
-	    var latLng = {
-	        lat: parseFloat( lat ),
-	        lng: parseFloat( lng )
-	    };
-	
-	   // Create marker instance.
+	    var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+		// Create marker instance.
 	    var marker = new google.maps.Marker({
-	        position : latLng,
+	        position : latlng,
 	        map: map,
 			icon: {
-				url: window.location.protocol + '//' + window.location.host + '/wp-content/themes/blackbay/includes/img/black-pin.png'
+				url: window.location.protocol + '//' + window.location.host + '/wp-content/themes/blackbay/includes/img/black-pin.png',
+				size: new google.maps.Size(32, 32),
+				origin: new google.maps.Point(0, 0),
 		    }
 	    });
 	
-	    // Append to reference for later use.
-	    map.markers.push( marker );
+		// add to array
+		map.markers.push( marker );
 	
-	    // If marker contains HTML, add it to an infoWindow.
-	    if( $marker.html() ){
-	
-	        // Create info window.
+		// if marker contains HTML, add it to an infoWindow
+		if( $marker.html() ) {
+			
+			// Create info window.
 	        var infowindow = new google.maps.InfoWindow({
 	            content: $marker.html()
 	        });
@@ -153,7 +151,7 @@
 		        infowindow.open(map, marker);
 		        activeInfoWindow = infowindow;
 			});
-	    }
+		}
 	}
 	
 	function markerCluster( markers, map ) {
@@ -164,34 +162,33 @@
 	
 	function centerMap( map ) {
 	
-	    // Create map boundaries from all map markers.
 	    var bounds = new google.maps.LatLngBounds();
-	    
+
+		// loop through all markers and create bounds
 		$.each( map.markers, function( i, marker ){
 			var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
 			bounds.extend( latlng );
 		});
 	
-	    // Case: Single marker.
-	    if( map.markers.length == 1 ){
-	        map.setCenter( bounds.getCenter() );
-	
-	    // Case: Multiple markers.
-	    } else{
-	        map.fitBounds( bounds );
-	    }
-		
-		// Set zoom level
-	    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-	        this.setZoom(4);
-	        google.maps.event.removeListener(boundsListener);
-	    });
+		// only 1 marker?
+		if( map.markers.length == 1 ) {
+			// set center of map
+		    map.setCenter( bounds.getCenter() );
+		    map.setZoom( 16 );
+		} else {
+			// fit to bounds
+			map.setCenter( bounds.getCenter() );
+		   	map.setZoom( 2 ); // Change the zoom value as required
+			//map.fitBounds( bounds ); // This is the default setting which I have uncommented to stop the World Map being repeated
+		}
 	}
 	
+	var map = null;
+	
 	// Render maps on page load.
-	$(document).ready(function(){
+	$function(){
 	    $('.acf-map').each(function(){
-	        var map = initMap( $(this) );
+	        map = new_map( $(this) );
 	    });
 	});
 
@@ -199,11 +196,19 @@
 </script>
 
 <?php 
-$loop = new WP_Query( array( 
+$args = array( 
+	'numberposts' => -1,
 	'post_type' => 'residential', 
-	'posts_per_page' => -1 
-	) 
+	'order' => 'DESC',
+	'meta_query'=> array(
+		array(
+            'key' => 'exclude_from_list',
+            'value' => '1',
+            'compare' => '!='
+        )
+	)
 );
+$loop = new WP_Query( $args );
 
 if ( $loop->have_posts() ) : ?>
 <div class="acf-map-container">
