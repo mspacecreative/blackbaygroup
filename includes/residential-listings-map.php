1d3f5c54dd499a8bcfe5309a6795938fc2e273ec
Matt <matt@mspacecreative.com>
2021-05-06 13:07:47 -0300

    c

--- includes/residential-listings-map.php
+++ includes/residential-listings-map.php
@@ -83,71 +83,82 @@
 <script>
 (function($) {
 
-	function initMap( $el ) {
-	
-	    // Find marker elements within map.
-	    var $markers = $el.find('.marker');
-	
-	    // Create gerenic map.
-	    var mapArgs = {
-	        zoom        : $el.data('zoom') || 16,
-	        mapTypeId   : google.maps.MapTypeId.ROADMAP
-	    };
-	    var map = new google.maps.Map( $el[0], mapArgs );
-	
-	    // Add markers.
-	    map.markers = [];
-	    $markers.each(function(){
-	        initMarker( $(this), map );
-	    });
-	
-	    // Center map based on markers.
-	    centerMap( map );
-		
-		// add marker cluster
-		markerCluster( map.markers, map );
-	
-	    // Return map instance.
-	    return map;
-	}
+	function new_map( $el ) {
+
+	// var
+	var $markers = $el.find('.marker');
+
+
+	// vars
+	var args = {
+		zoom		: 16,
+		center		: new google.maps.LatLng(0, 0),
+		mapTypeId	: google.maps.MapTypeId.ROADMAP
+	};
+
+
+	// create map
+	var map = new google.maps.Map( $el[0], args);
+
+
+	// add a markers reference
+	map.markers = [];
+
+
+	// add markers
+	$markers.each(function(){
+
+    	add_marker( $(this), map );
+
+	});
+
+
+	// center map
+	center_map( map );
+
+
+	// return
+	return map;
+
+}
 	
 	var activeInfoWindow;
-	function initMarker( $marker, map ) {
-	
-	    // Get position from marker.
-	    var lat = $marker.data('lat');
-	    var lng = $marker.data('lng');
-	    var latLng = {
-	        lat: parseFloat( lat ),
-	        lng: parseFloat( lng )
-	    };
-	
-	   // Create marker instance.
-	    var marker = new google.maps.Marker({
-	        position : latLng,
-	        map: map,
-			icon: {
-				url: window.location.protocol + '//' + window.location.host + '/wp-content/themes/blackbay/includes/img/black-pin.png'
-		    }
-	    });
-	
-	    // Append to reference for later use.
-	    map.markers.push( marker );
-	
-	    // If marker contains HTML, add it to an infoWindow.
-	    if( $marker.html() ){
-	
-	        // Create info window.
-        var infowindow = new google.maps.InfoWindow({
-            content: $marker.html()
-        });
+	function add_marker( $marker, map ) {
 
-        // Show info window when marker is clicked.
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open( map, marker );
-        });
-	    }
+	// var
+	var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+	// create marker
+	var marker = new google.maps.Marker({
+		position	: latlng,
+		map			: map
+	});
+
+	// add to array
+	map.markers.push( marker );
+
+	// if marker contains HTML, add it to an infoWindow
+	if( $marker.html() )
+	{
+		// Create info window.
+	        var infowindow = new google.maps.InfoWindow({
+	            content: $marker.html()
+	        });
+	
+	        // Show info window when marker is clicked.
+		    google.maps.event.addListener(marker, 'click', function() {
+		        map.panTo(this.getPosition());
+				//map.panBy(0,-100);
+				if (activeInfoWindow) { 
+					activeInfoWindow.close();
+				}
+		        infowindow.open(map, marker);
+		        activeInfoWindow = infowindow;
+			});
 	}
+
+}
+	
 	
 	function markerCluster( markers, map ) {
 	    var markerCluster = new MarkerClusterer(map, markers, {
@@ -155,38 +166,51 @@
 		});
 	}
 	
-	function centerMap( map ) {
-	
-	    // Create map boundaries from all map markers.
-	    var bounds = new google.maps.LatLngBounds();
-	    
-		$.each( map.markers, function( i, marker ){
-			var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-			bounds.extend( latlng );
-		});
-	
-	    // Case: Single marker.
-	    if( map.markers.length == 1 ){
-	        map.setCenter( bounds.getCenter() );
-	
-	    // Case: Multiple markers.
-	    } else{
-	        map.fitBounds( bounds );
-	    }
-		
-		// Set zoom level
-	    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-	        this.setZoom(4);
-	        google.maps.event.removeListener(boundsListener);
-	    });
-	}
-	
-	// Render maps on page load.
-	$(document).ready(function(){
-	    $('.acf-map').each(function(){
-	        var map = initMap( $(this) );
-	    });
+	function center_map( map ) {
+
+	// vars
+	var bounds = new google.maps.LatLngBounds();
+
+	// loop through all markers and create bounds
+	$.each( map.markers, function( i, marker ){
+
+		var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+		bounds.extend( latlng );
+
 	});
+
+	// only 1 marker?
+	if( map.markers.length == 1 )
+	{
+		// set center of map
+	    map.setCenter( bounds.getCenter() );
+	    map.setZoom( 16 );
+	}
+	else
+	{
+		// fit to bounds
+		  map.setCenter( bounds.getCenter() );
+	   	map.setZoom( 2 ); // Change the zoom value as required
+		//map.fitBounds( bounds ); // This is the default setting which I have uncommented to stop the World Map being repeated
+
+	}
+
+}
+	
+	var map = null;
+
+jQuery(document).ready(function($){
+
+	$('.acf-map').each(function(){
+
+		// create map
+		map = new_map( $(this) );
+
+	});
+
+
+});
 
 })(jQuery);
 </script>
